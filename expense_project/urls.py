13f2e2ff591794c1e906a3eedd6195d484acf24a
jayjaychukwu <liljaydon.pj@gmail.com>
2022-09-05 00:24:37 +0100

    Added an endpoint for json endpoints collections

--- expense_project/urls.py
+++ expense_project/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.urls import re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
+from django.urls import include, path, re_path
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 ...
 
@@ -29,6 +28,11 @@ urlpatterns = [
     path(
         "",
         schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "api.json/",
+        schema_view.without_ui(cache_timeout=0),
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
